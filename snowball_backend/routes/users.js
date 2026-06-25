20c9a37fc345ab9e7922d9e7c7290711a76e5af0
Rohan2519Y <rohankumar17645@gmail.com>
2026-06-25 08:58:07 +0530

    Database alive fix

--- snowball_backend/routes/users.js
+++ snowball_backend/routes/users.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('./pool')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.json({
-    status: 'OK',
-    timestamp: new Date().toISOString(),
-  });
+  try {
+    pool.query("select 1;")
+    res.json({
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+    });
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 module.exports = router;
